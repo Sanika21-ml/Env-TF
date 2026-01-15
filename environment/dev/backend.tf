3549ecb292cb971c8b9ea8c3662b95ae5fda8643
Sanika <sanikakhire891@gmail.com>
2026-01-15 16:44:42 +0530

    Change sources

--- environment/dev/backend.tf
+++ environment/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "Backend-git-1"
-    storage_account_name = "storageaccountbackendgit1122"
+    storage_account_name = "storageaccountgit1122"
     container_name = "backendgit102" 
     key = "terraform.tfstate"
   }
