c4cde45388cd2e48c7926f12dc401f6eb0115cba
Sanika <sanikakhire891@gmail.com>
2026-01-15 16:47:42 +0530

    Add backend info

--- environment/dev/backend.tf
+++ environment/dev/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "Backend-git-1"
-    storage_account_name = "storageaccountgit1122"
-    container_name = "backendgit102" 
+    resource_group_name  = "Backend-git"
+    storage_account_name = "storageaccountbackendgit"
+    container_name = "backendgit" 
     key = "terraform.tfstate"
   }
 }
